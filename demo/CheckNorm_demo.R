ecc6fb55105815f55ac4da7ba64ce4cce2563ed6
lindgrenrebecca <rlindgren120@gmail.com>
2025-04-29 16:11:46 -0400

    update demo

--- demo/CheckNorm_demo.R
+++ demo/CheckNorm_demo.R
@@ -3,12 +3,22 @@ library(CheckNorm)
 
 #load simulated normal data from CheckNorm package
 data("normaldata")
+head(normaldata)
 
 #load "real" data from built in R datasets
 data("airquality")
+head(airquality)
 
-#Jump straight into examining data normality!
-check_all(normaldata)
+#Let's statistically evaluate the normality of the normaldata dataset
+stat_test(normaldata)
 
 #Same thing with the "real" data
+stat_test(airquality$Solar.R)
+
+#Now, let's visualize those results
+vis_check(normaldata)
+vis_check(airquality$Solar.R)
+
+#But instead of having to take two different steps to evaluate normality, let's do it all in one!
+check_all(normaldata)
 check_all(airquality$Solar.R)
